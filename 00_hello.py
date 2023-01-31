e99d772e6ffa1d2d281575281fa8ef92eb730b55
marki8 <marki8@hotmail.com>
2023-01-31 16:55:44 +0100

    First commit

---
+++ 00_hello.py
@@ -0,0 +1,22 @@
+# Comentario en una linea: Hola mundo
+print("Hola Python")
+print('Hola Python')
+
+"""
+Este es un comentario multilinea
+"""
+
+'''
+Este es otro comentario multilinea
+'''
+
+#Inferir tipo de dato
+print(type(10))
+print(type(10.0))
+print(type(10+1j))
+print(type("Hola"))
+print(type(False))
+print(type([1,2]))
+print(type((1,2,3)))
+print(type({"a","b","c"}))
+print(type({'uno':'casa','dos':'arbol'}))
---
+++ 01_variables.py
@@ -0,0 +1,6 @@
+# Variable name: avoid upper-case or camel-case. Use snake-case
+my_string_variable = "This is my variable"
+print(my_string_variable)
+
+my_int_variable = 1
+print(my_string_variable, my_int_variable)
